e4f4136e7433433afb84ee7611c546e51113a93f
Aiden Brent <aidenjb81@gmail.com>
2024-06-21 05:27:33 +0000

    Fixing incorrect function call.

--- lambda_handler.py
+++ lambda_handler.py
@@ -13,11 +13,12 @@ def lambda_handler(event, context):
     topic_df = etl.create_df(zip_links['gkg'])
 
     scores_file = 'day-scores.parquet'
-    scores_df = etl.create_scores_df(S3_BUCKET, scores_file)
+    scores_df = utils.df_from_bucket(S3_BUCKET, scores_file)
 
     dead_time = datetime.datetime.strptime(curr_dt, '%Y%m%d%H%M%S') - datetime.timedelta(days=1)
     old_file = dead_time.strftime('%Y%m%d%H%M%S') + '-scores.parquet'
     old_df = utils.df_from_bucket(S3_BUCKET, old_file)
+
     combine_df = combine_topics.combine_df_topics(topic_df, old_df)
     scores_updated = etl.update_scores(combine_df, scores_df, S3_BUCKET, old_df)
     success = etl.upload(combine_df, S3_BUCKET, curr_dt)
