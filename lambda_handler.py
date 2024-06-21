423cf467ba38a5308c3c41871e0db2a1447aa4ea
Aiden Brent <aidenjb81@gmail.com>
2024-06-21 05:19:05 +0000

    Adding missing parameter.

--- lambda_handler.py
+++ lambda_handler.py
@@ -11,7 +11,6 @@ S3_BUCKET = 'gdelt-data-prod'
 def lambda_handler(event, context):
     curr_dt, zip_links = etl.get_zip_links()
     topic_df = etl.create_df(zip_links['gkg'])
-    combine_df = combine_topics.combine_df_topics(topic_df)
 
     scores_file = 'day-scores.parquet'
     scores_df = etl.create_scores_df(S3_BUCKET, scores_file)
@@ -19,6 +18,7 @@ def lambda_handler(event, context):
     dead_time = datetime.datetime.strptime(curr_dt, '%Y%m%d%H%M%S') - datetime.timedelta(days=1)
     old_file = dead_time.strftime('%Y%m%d%H%M%S') + '-scores.parquet'
     old_df = utils.df_from_bucket(S3_BUCKET, old_file)
+    combine_df = combine_topics.combine_df_topics(topic_df, old_df)
     scores_updated = etl.update_scores(combine_df, scores_df, S3_BUCKET, old_df)
     success = etl.upload(combine_df, S3_BUCKET, curr_dt)
     if not (success and scores_updated):
