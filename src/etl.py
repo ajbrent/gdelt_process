495a697b975da257cc4378093becf340fc5c66f6
Aiden Brent <aidenjb81@gmail.com>
2024-06-30 01:55:41 +0000

    filtering before calculation.

--- src/etl.py
+++ src/etl.py
@@ -274,9 +274,9 @@ def update_scores(new_data: pd.DataFrame, scores_df: pd.DataFrame, bucket: str, 
 
     merge_df['day_counts'] = merge_df['day_counts'] - merge_df['counts']
     merge_df['day_src_counts'] = merge_df['day_src_counts'] - merge_df['src_counts']
+    merge_df = merge_df[merge_df['day_counts'] > 0]
     merge_df['day_scores'] = merge_df.apply(score_func, axis=1)
     merge_df = merge_df.drop(columns=['counts', 'src_counts'])
-    merge_df = merge_df[merge_df['day_counts'] > 0]
     
     _ = client.put_object(Bucket=bucket, Key='day-scores.parquet', Body=merge_df.to_parquet())
     return True
